00b6703e0d0fce445268a43fadfdfa9e3a0907da
Nikila Selvasekar <nikiofz11@gmail.com>
2026-09-10 20:54:51 +0530

    Add HackerRank Easy - Welcome to Java!

---
+++ HackerRank/Easy/Welcome to Java!/solution.java
@@ -0,0 +1,12 @@
+// HackerRank Problem: Welcome to Java!
+// Link: https://www.hackerrank.com/challenges/welcome-to-java/problem
+// Difficulty: Easy
+// Language: java
+
+public class Solution {
+
+    public static void main(String[] args) {
+      System.out.println("Hello, World.");
+      System.out.println("Hello, Java.");
+}
+}
