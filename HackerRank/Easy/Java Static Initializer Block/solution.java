92d075e49773d7683276cf61f9d63e46e9706f0d
Nikila Selvasekar <nikiofz11@gmail.com>
2026-09-10 20:53:34 +0530

    Add HackerRank Easy - Java Static Initializer Block

---
+++ HackerRank/Easy/Java Static Initializer Block/solution.java
@@ -0,0 +1,32 @@
+// HackerRank Problem: Java Static Initializer Block
+// Link: https://www.hackerrank.com/challenges/java-static-initializer-block/problem
+// Difficulty: Easy
+// Language: java
+
+
+    
+       
+    static int B;
+    static int H;
+    static boolean flag = true;
+    
+    static
+    {
+        Scanner sc = new Scanner(System.in);
+        
+         B = sc.nextInt();
+        H = sc.nextInt();
+    
+             
+              if (B <= 0 || H <= 0)
+               {
+                    flag = false;
+                   System.out.println("java.lang.Exception: Breadth and height must be positive");
+               }
+               
+    }
+    
+
+              
+
+
